b0bafc31565601886c222f475adfdd10d3ffbab7
Timo Kubera <t.kubera@reply.de>
2024-03-11 10:56:41 +0100

    Setup for local testing

--- code_quality_agent/tests/File1.java
+++
@@ -1,8 +0,0 @@
-public final class File1 {
-
-    private File1() {}
-
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
-}
--- code_quality_agent/tests/File3.java
+++
@@ -1,7 +0,0 @@
-public class File3 {
-    public void doSomething() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i);
-        }
-    }
-}
