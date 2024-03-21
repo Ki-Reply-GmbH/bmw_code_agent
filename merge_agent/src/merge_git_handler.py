5b604a4e8d5860ea8f1441934a8d68bd7c4f9c6c
Timo Kubera <t.kubera@reply.de>
2024-03-21 17:28:24 +0100

    Using remote target and source branch

--- merge_agent/src/merge_git_handler.py
+++ merge_agent/src/merge_git_handler.py
@@ -120,7 +120,7 @@ class MergeGitHandler(GitHandler):
             bool: False if the merge is successful, True if a GitCommandError is raised.
         """
         target_branch = self._target_branch
-        target_branch_ref = getattr(self._repo.refs, target_branch_ref)
+        target_branch_ref = getattr(self._repo.refs, target_branch)
         try:
             self._repo.git.merge(target_branch_ref)
             return False
