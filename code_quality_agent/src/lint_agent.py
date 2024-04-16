9cceba940b122031eeac65d8a9a8cc71ff7f7ace
Timo Kubera <t.kubera@reply.de>
2024-04-16 17:08:44 +0200

    update_progress_bar to create_...

--- code_quality_agent/src/lint_agent.py
+++ code_quality_agent/src/lint_agent.py
@@ -162,7 +162,7 @@ class LintAgent(CodeQualityAgent):
         """
         if self.language in self.highlighted_languages:
             for n, task in enumerate(self.tasks):
-                pr_git_handler.update_progress_bar(index + n * increment)
+                pr_git_handler.create_progress_bar(index + n * increment)
                 file_path, task_description = task
                 LOGGER.debug("Improving " + file_path + "...")
                 with open(file_path, "r") as file:
@@ -176,7 +176,7 @@ class LintAgent(CodeQualityAgent):
                 self.improved_source_code.append((file_path, improved_source_code))
         else:
             for n, file in enumerate(self.file_list):
-                pr_git_handler.update_progress_bar(index + n * increment)
+                pr_git_handler.create_progress_bar(index + n * increment)
                 file_path = os.path.join(self.directory, file)
                 with open(file_path, "r") as file:
                     code = file.read()
