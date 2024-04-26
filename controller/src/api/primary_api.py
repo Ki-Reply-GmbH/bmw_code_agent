a645fb519285ac8569912cdfc01f85b78bf4f2f4
Timo Kubera <t.kubera@reply.de>
2024-04-26 11:49:28 +0200

    Deleting webhook blueprint

--- controller/src/api/primary_api.py
+++ controller/src/api/primary_api.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from controller.src.api.webhook import webhook_blueprint
 from controller.src.api.change_config import change_config_blueprint
 
 app = Flask(__name__)
-app.register_blueprint(webhook_blueprint)
 app.register_blueprint(change_config_blueprint)
 
 if __name__ == "__main__":
